6b7f4383bdbb61976f34391aade2dfc8ba803215
Jesse Ragos <jkiel.r4gos@gmail.com>
2019-03-02 11:32:47 +0800

    Remove cors

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const Pusher = require("pusher");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const shortId = require("shortid");
 const dialogFlow = require("./dialogFlow");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -77,4 +77,4 @@ if (process.env.NG_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+app.listen(PORT, () => console.log(`Listening at Port: ${PORT}`));
